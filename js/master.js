045b08e7b9e5adad8b3f161fc94d1efde36ff9e8
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2021-05-03 22:02:42 +0200

    adding toggle open sideDrawer

--- js/master.js
+++ js/master.js
@@ -1,3 +1,10 @@
+// toggle spin class on Item
+document.querySelector('.toggle-settings .fa-gear').onclick = function () {
+    //toggle class  fa-spin for rottation onself
+    this.classList.toggle("fa-spin")
+    // toggle Class Open on Main Settings Box
+    document.querySelector(".settings-box").classList.toggle('open')
+}
 const landingPage = document.querySelector('.landing-page')
 
 const imagesArray = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg']
