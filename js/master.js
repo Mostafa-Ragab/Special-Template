0075b58d0a5dba094ac49f79a8e5ea36e7aed902
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2021-07-26 11:33:25 +0200

    adding colors list

--- js/master.js
+++ js/master.js
@@ -5,6 +5,18 @@ document.querySelector('.toggle-settings .fa-gear').onclick = function () {
     // toggle Class Open on Main Settings Box
     document.querySelector(".settings-box").classList.toggle('open')
 }
+// Switch colors
+ const colorsLi = document.querySelectorAll(".colors-list li")
+//  Loop On All List
+
+.forEach(li => {
+    // Click on Every list Items
+    li.addEventListener('click', (e) => {
+        
+        // Set Color On Root
+        document.documentElement.style.setProperty('--main-color',e.target.dataset.color)
+    })
+});
 const landingPage = document.querySelector('.landing-page')
 
 const imagesArray = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg']
